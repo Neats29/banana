9c82520ef1a1503dd23ca49e535ca704d246bc10
Neats29 <baadge29@yahoo.co.uk>
2016-02-19 11:13:25 +0000

    upload works but want to modularise it

--- ionic/features/step_definitions/profile_steps.js
+++ ionic/features/step_definitions/profile_steps.js
@@ -1,11 +1,16 @@
+var assert = require('assert');
+
 module.exports = function () {
+  
   this.Given(/^I am a user$/, function (callback) {
     // Write code here that turns the phrase above into concrete actions
     callback.pending();
   });
 
   this.When(/^I navigate to '\/profile'$/, function (callback) {
-    // Write code here that turns the phrase above into concrete actions
+//    it('should add Hello to the name', function() {
+//  expect(element(by.binding("greeting")).getText()).toEqual('Bonjour World!');
+//});
     callback.pending();
   });
 
---
+++ ionic/www/js/upload.js
@@ -0,0 +1,48 @@
+angular.module('upload', [])
+  .directive('image', function($q) {
+    'use strict';
+    var URL = window.URL || window.webkitURL;
+    var createImage = function(url, callback) {
+      var image = new Image();
+      image.onload = function() {
+        callback(image);
+      };
+      image.src = url;
+    };
+
+    var fileToDataURL = function (file) {
+      var deferred = $q.defer();
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          deferred.resolve(e.target.result);
+      };
+      reader.readAsDataURL(file);
+      return deferred.promise;
+    };
+
+    return {
+      link: function postLink(scope, element) {
+          var applyScope = function(imageResult) {
+            scope.$apply(function() {
+              scope.image = imageResult; 
+            });
+          };
+
+          element.bind('change', function (event) {
+            var files = event.target.files;
+            var imageResult = {
+              file: files[0],
+              url: URL.createObjectURL(files[0])
+            };
+
+            fileToDataURL(files[0]).then(function (dataURL) {
+                imageResult.dataURL = dataURL;
+            });
+
+            applyScope(imageResult);      
+          });
+        }
+    };
+});
+
+    
