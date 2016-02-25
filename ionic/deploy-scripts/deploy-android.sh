4bd96dc19ff78584d09e166ac43bfddebd2d7bda
Andrew.Dick <andrew.dick@cohaesus.co.uk>
2016-02-25 11:51:35 +0000

    android-deploy tweaks

--- ionic/deploy-scripts/deploy-android.sh
+++ ionic/deploy-scripts/deploy-android.sh
@@ -5,6 +5,7 @@ current_tag=$(git describe --abbrev=0 --tags)
 echo "Current tag: $current_tag"
 
 # push the app to HockeyApp
+# API parameters: http://support.hockeyapp.net/kb/api/api-apps#upload-app
 response=$(curl \
   -F "status=2" \
   -F "notify=1" \
@@ -15,7 +16,7 @@ response=$(curl \
 
 echo $response
 
-publink=$(echo $response | json public_url)
+publink=$(echo $response)
 
 msg="Android Build $current_tag published: $publink"
 
@@ -30,3 +31,26 @@ echo $(curl \
 	--data-urlencode "message=$MESSAGE"	\
 	"https://api.hipchat.com/v1/rooms/message?auth_token=${AUTH_TOKEN}&format=json"
 	)
+
+
+
+  {"apps":[{
+    "title":"ionic_experiment",
+    "bundle_identifier":"com.ionicframework.ionicexperiment798537",
+    "public_identifier":"26809419b03d4fc880dcc3334a71851f",
+    "platform":"Android",
+    "release_type":0,
+    "custom_release_type":null,
+    "created_at":"2016-02-24T14:08:38Z",
+    "updated_at":"2016-02-25T11:14:49Z",
+    "featured":false,
+    "id":289488,
+    "minimum_os_version":"4.1",
+    "device_family":null,
+    "status":2,
+    "visibility":"private",
+    "owner":"Andrew Dick",
+    "owner_token":"505913c2abecca01d7fc88988d09651759bb2e9d",
+    "company":"Cohaesus"}],
+
+    "status":"success"}
