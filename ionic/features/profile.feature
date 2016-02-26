64bb626c047b3adaa52cb52e1fd2bed9a0aa84e1
Jamal Osman <JamalOsman@Cohaesuss-MacBook-Pro-8.local>
2016-02-26 18:02:14 +0000

    rewrite profile.featureand regenerate step definitions

--- ionic/features/profile.feature
+++ ionic/features/profile.feature
@@ -1,13 +1,23 @@
 Feature: profile
 
 As a user
-I want to enter my name
+I want to enter my firstname
+I want to enter my lastname
+I want to enter my email
+I want to enter my phonenumber
+I want to enter my skills
 I want to upload my photo
+  
+Scenario: Visit the profile page
+  When I navigate to '/profile'
+  Then I should arrive at the profile page
   
 Background:
   Given I am on the profile page
   
-Scenario: Submit profile form
-  When I click the save button
-  Then I should be taken to '/profile/view'
-  
+Scenario:
+  Then I should see inputs to enter my details
+  When I enter my details
+  And I click the save button
+  Then my details should be saved to the server
+  And I should be taken to '/profile/view'  
--- ionic/features/step_definitions/profile_steps.js
+++ ionic/features/step_definitions/profile_steps.js
@@ -9,37 +9,47 @@ var expect = chai.expect;
 
 var baseURL = 'http://172.16.32.143:8100/#/app';
 
-
+// TODO: Write tests
 module.exports = function () {
 
-    this.Given(/^I am on the profile page$/, function (next) {
-        var url = baseURL + '/profile';
-        browser.get(url);
-        browser.getCurrentUrl().then(function (url) {
-            console.log('Browser Current URL', url);
-            next();
-        });
+  this.When(/^I navigate to '\/profile'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
+  this.Then(/^I should arrive at the profile page$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
-    });
+  this.Given(/^I am on the profile page$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
-    this.When(/^I click the save button$/, function (next) {
-        var saveButton = browser.findElement({
-            id: 'saveButton'
-        });
+  this.Then(/^I should see inputs to enter my details$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
-        saveButton.click().then(function () {
-            console.log('Click Callback parameter');
+  this.When(/^I enter my details$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
-            next();
-        });
-    });
+  this.When(/^I click the save button$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
-    this.Then(/^I should be taken to '\/profile\/view'$/, function (callback) {
-        browser.getCurrentUrl().then(function (url) {
-            console.log('THEN: Browser Current URL', url);
-            expect(url).to.equal(baseURL + '/profile/view');
-            callback();
-        });
-    });
+  this.Then(/^my details should be saved to the server$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^I should be taken to '\/profile\/view'$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
 };
